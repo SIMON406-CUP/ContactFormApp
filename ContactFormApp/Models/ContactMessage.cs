8ca8a0c9d7f5b3dad73ff87bed05c8ee55104a9a
Moriswi Simon <simonmothoka40@gmail.com>
2026-02-03 15:04:44 +0200

    added ContactMessage model and database context

---
+++ ContactFormApp/Models/ContactMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace ContactFormApp.Models
+{
+    public class ContactMessage
+    {
+        public int Id { get; set; }  // primary key
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Message { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
