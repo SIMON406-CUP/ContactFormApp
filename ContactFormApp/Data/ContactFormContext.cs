be9debb9df4d7590ec1fe4e5d8cc2398e4bfb37d
Moriswi Simon <simonmothoka40@gmail.com>
2026-02-03 15:07:05 +0200

    added ContactFormContext to handle database connections

---
+++ ContactFormApp/Data/ContactFormContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContactFormApp.Models;
+
+namespace ContactFormApp.Data
+{
+    public class ContactFormContext : DbContext
+    {
+        public ContactFormContext(DbContextOptions<ContactFormContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ContactMessage> ContactMessages { get; set; }
+    }
+}
